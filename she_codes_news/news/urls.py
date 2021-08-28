320725b1bbda33707db5aa2d43dfd0e6e34cdc96
shelleybehen <shelleybehen@gmail.com>
2021-08-28 20:02:55 +1000

    css

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'news'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'), #/news/
-    path('<int:pk>/', views.StoryView.as_view(), name='story'), #/news/4
-    path('add-story/', views.AddStoryView.as_view(), name='newStory') #where we had our form to add a new story
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.StoryView.as_view(), name='story'), 
+    path('add-story/', views.AddStoryView.as_view(), name='newStory') 
 ]
