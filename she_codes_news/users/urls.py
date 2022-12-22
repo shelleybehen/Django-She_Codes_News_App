9fd252e39bb4a31e985aa73c6d29537fbf1dc68f
shelleybehen <shelleybehen@gmail.com>
2022-12-22 11:27:29 +1000

    Update urls.py

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('create-account/', CreateAccountView.as_view(),name='createAccount'),
     path('<int:pk>/',UserProfile.as_view(),name='userProfile')
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
